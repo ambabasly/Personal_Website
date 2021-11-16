8a418c6f76dc7326db953ad9447688d185498ccb
Sylvester Arikhan <arikhansylvester@gmail.com>
2021-11-16 16:15:12 +0100

    Updating resume.js

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -60,7 +60,7 @@ const Resume = (props) => {
     { label: "Work History", logoSrc: "work-history.svg" },
     { label: "Programming Skills", logoSrc: "programming-skills.svg" },
     { label: "Projects", logoSrc: "projects.svg" },
-    { label: "Projects2", logoSrc: "projects.svg" },
+    { label: "Projects", logoSrc: "projects.svg" },
     { label: "Education", logoSrc: "education.svg" },
   ];
 
